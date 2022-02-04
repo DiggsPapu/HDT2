97466ab88a2af8cab041b458ce93afd7d8fa3a30
Windows 10 <Windows 10@DESKTOP-65MKIDD>
2022-02-04 01:39:42 -0600

    -postfixcalculator.

--- PostfixCalculator/src/calculator/postfixCalculator.java
+++ PostfixCalculator/src/calculator/postfixCalculator.java
@@ -1,6 +1,6 @@
 package calculator;
 
-public class postfixCalculator<N> implements IPostfixCalculator<N> {
+public class postfixCalculator implements IPostfixCalculator {
 	public N validateValue(N val) {
 		try {
 			
@@ -20,35 +20,15 @@ public class postfixCalculator<N> implements IPostfixCalculator<N> {
 		return val;
 			
 	}
-	public void operationVerification(StackVector vector) {
-		if(vector.getCounterNumber()==1||vector.getCounterNumber()==0) {
+	public void operationVerification() {
+		if(getCounterNumber()==1||getCounterNumber()==0) {
 			System.out.print("Es inválida la operación postfix dado que no hay suficientes números para realizar dicha operación");
 		}
 		else {
-			vector.getHead().getValue();
-			vector.pop();
-			vector.getHead().getValue();
-			vector.pop();
+			getHead().getValue();
+			pop();
+			getHead().getValue();
+			pop();
 		}
-	}
-	@Override
-	public int addition(N value1, N value2) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	@Override
-	public int substract(N value1, N value2) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	@Override
-	public int divide(N value1, N value2) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	@Override
-	public int sendResult() {
-		// TODO Auto-generated method stub
-		return 0;
 	}
 }
